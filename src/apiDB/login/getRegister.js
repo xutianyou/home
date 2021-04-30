422f3367bd5c0001956d764f9743afe5054af0bd
xutianyou <94401785@qq.com>
2021-04-30 17:24:15 +0800

    格式化

--- src/apiDB/login/getRegister.js
+++ src/apiDB/login/getRegister.js
@@ -1,87 +1,87 @@
 import DB from '../indexedDB'
 
 async function service (data) {
-	let msg = '', hasList, hasName = false, isAdd, setUserData;
-	let isOpen = await DB.openDB("xtyDB", 'UserInformationTable', 1);
+  let msg = '', hasList, hasName = false, isAdd, setUserData;
+  let isOpen = await DB.openDB("xtyDB", 'UserInformationTable', 1);
 
-	if (isOpen) {
-		// 获取账号列表信息
-		hasList = await DB.getAllData(isOpen, 'UserInformationTable');
+  if (isOpen) {
+    // 获取账号列表信息
+    hasList = await DB.getAllData(isOpen, 'UserInformationTable');
 
-		if (hasList) {
-			hasName = hasList.some(item => item.name === data.name);
+    if (hasList) {
+      hasName = hasList.some(item => item.name === data.name);
 
-			if (!hasName) {
-				// UserInformationTable表添加新用户登录账号密码
-				isAdd = await DB.setData(isOpen, 'UserInformationTable', { 
-					name: data.name,
-					password: data.password
-				});
+      if (!hasName) {
+        // UserInformationTable表添加新用户登录账号密码
+        isAdd = await DB.setData(isOpen, 'UserInformationTable', { 
+          name: data.name,
+          password: data.password
+        });
 
-				if (isAdd) {
-					// 为新用户新建独立资料库，库名为用户账号
-					let isOpenUser = await DB.openDB(data.name, [{
-						name: 'user',
-						data: {
-							keyPath: 'id',
-							autoIncrement: true
-						}
-					},{
-						name: 'data',
-						data: {
-							keyPath: 'id',
-							autoIncrement: true
-						}
-					}], 1);
+        if (isAdd) {
+          // 为新用户新建独立资料库，库名为用户账号
+          let isOpenUser = await DB.openDB(data.name, [{
+            name: 'user',
+            data: {
+              keyPath: 'id',
+              autoIncrement: true
+            }
+          },{
+            name: 'data',
+            data: {
+              keyPath: 'id',
+              autoIncrement: true
+            }
+          }], 1);
 
-					if (isOpenUser) {
-						// user表添加新用户所有基本信息
-						setUserData = await DB.setData(isOpenUser, 'user', { 
-							name: data.name,
-							password: data.password,
-							theme: 'blue',
-							img: ''
-						});
-					} else {
-						console.log('新增用户个人信息库失败');
-					}
-				}
+          if (isOpenUser) {
+            // user表添加新用户所有基本信息
+            setUserData = await DB.setData(isOpenUser, 'user', { 
+              name: data.name,
+              password: data.password,
+              theme: 'blue',
+              img: ''
+            });
+          } else {
+            console.log('新增用户个人信息库失败');
+          }
+        }
 
-				DB.close(isOpen);
-				isOpen = null;
+        DB.close(isOpen);
+        isOpen = null;
 
-				if (isAdd && setUserData) {
-					msg = '新增用户成功';
-				} else {
-					msg = '新增数据插入失败';
-				}
-			} else {
-				msg = '用户名已存在';
-			}
-		} else {
-			msg = '数据查询失败';
-		}
-	} else {
-		msg = '数据库连接失败';
-	}
+        if (isAdd && setUserData) {
+          msg = '新增用户成功';
+        } else {
+          msg = '新增数据插入失败';
+        }
+      } else {
+        msg = '用户名已存在';
+      }
+    } else {
+      msg = '数据查询失败';
+    }
+  } else {
+    msg = '数据库连接失败';
+  }
 
-	return new Promise((resolver, reject) => {
-		if (isAdd) {
-			resolver({
-				code: '00',
-				msg
-			})
-		} else if (hasName){
-			resolver({
-				code: '01',
-				msg
-			})
-		} else {
-			reject({
-				code: '9999',
-				msg
-			})
-		}
-	})
+  return new Promise((resolver, reject) => {
+    if (isAdd) {
+      resolver({
+        code: '00',
+        msg
+      })
+    } else if (hasName){
+      resolver({
+        code: '01',
+        msg
+      })
+    } else {
+      reject({
+        code: '9999',
+        msg
+      })
+    }
+  })
 }
 export default service
--- src/router/loginMenu.js
+++ src/router/loginMenu.js
@@ -1,110 +1,110 @@
 const menu = [
-	{
-		label: '简历',
-		name: 'ProjectBrief',
-		path: '/project-brief',
-		icon: 'icon-resume'
-	},
-	{
-		label: '开发工具',
-		icon: 'icon-tool',
-		children: [
-			{
-				label: 'API',
-				name: 'ToolAPI',
-				path: '/tool/api'
-			}
-		]
-	}, 
-	{
-		label: '项目文档',
-		icon: 'icon-file',
-		children: [
-			{
-				label: '路由配置',
-				name: 'RoutingConfiguration',
-				path: '/file/routing-configuration'
-			},
-			{
-				label: '多主题一键切换',
-				name: 'ThemeSwitch',
-				path: '/file/theme-switch'
-			},
-			{
-				label: '屏幕自适应',
-				name: 'ScreenAdaptation',
-				path: '/file/screen-adaptation'
-			},
-			{
-				label: 'style-deep',
-				name: 'StyleDeep',
-				path: '/file/style-deep'
-			},
-			{
-				label: '全局less',
-				name: 'GlobalLess',
-				path: '/file/global-less'
-			}
-		]
-	}, 
   {
-		label: '组件',
-		icon: 'icon-warehouse',
+    label: '简历',
+    name: 'ProjectBrief',
+    path: '/project-brief',
+    icon: 'icon-resume'
+  },
+  {
+    label: '开发工具',
+    icon: 'icon-tool',
     children: [
       {
-				label: '复制xlsx表格数据',
-				name: 'CopyData',
+        label: 'API',
+        name: 'ToolAPI',
+        path: '/tool/api'
+      }
+    ]
+  }, 
+  {
+    label: '项目文档',
+    icon: 'icon-file',
+    children: [
+      {
+        label: '路由配置',
+        name: 'RoutingConfiguration',
+        path: '/file/routing-configuration'
+      },
+      {
+        label: '多主题一键切换',
+        name: 'ThemeSwitch',
+        path: '/file/theme-switch'
+      },
+      {
+        label: '屏幕自适应',
+        name: 'ScreenAdaptation',
+        path: '/file/screen-adaptation'
+      },
+      {
+        label: 'style-deep',
+        name: 'StyleDeep',
+        path: '/file/style-deep'
+      },
+      {
+        label: '全局less',
+        name: 'GlobalLess',
+        path: '/file/global-less'
+      }
+    ]
+  }, 
+  {
+    label: '组件',
+    icon: 'icon-warehouse',
+    children: [
+      {
+        label: '复制xlsx表格数据',
+        name: 'CopyData',
         path: '/components/copy-data'
       }
     ]
   },
-	{
-		label: 'element',
-		icon: 'icon-word',
-		children: [
-			{
-				label: '下拉框',
-				name: 'ElementSelect',
-				path: '/element/select'
-			}, 
-			{
-				label: '表格',
-				name: 'ElementTable',
-				path: '/element/table'
-			}, 
-			{
-				label: '主题',
-				name: 'ElementTheme',
-				path: '/element/theme'
-			}
-		]
-	}, 
-	{
-		label: 'echarts',
-		icon: 'icon-chart',
-		children: [
-			{
-				label: '柱状图',
-				name: 'EchartsBar',
-				path: '/echarts/bar'
-			}
-		]
-	}, 
-	{
-		label: '卡片',
-		icon: 'icon-shuzihua',
-		children: [
-			{
-				label: '卡片数据录入',
-				name: 'CardData',
-				path: '/card/data'
-			},
-			{
-				label: '创建卡片',
-				name: 'CardAdd',
-				path: '/card/add'
-			}
-		]
-	}
+  {
+    label: 'element',
+    icon: 'icon-word',
+    children: [
+      {
+        label: '下拉框',
+        name: 'ElementSelect',
+        path: '/element/select'
+      }, 
+      {
+        label: '表格',
+        name: 'ElementTable',
+        path: '/element/table'
+      }, 
+      {
+        label: '主题',
+        name: 'ElementTheme',
+        path: '/element/theme'
+      }
+    ]
+  }, 
+  {
+    label: 'echarts',
+    icon: 'icon-chart',
+    children: [
+      {
+        label: '柱状图',
+        name: 'EchartsBar',
+        path: '/echarts/bar'
+      }
+    ]
+  }, 
+  {
+    label: '卡片',
+    icon: 'icon-shuzihua',
+    children: [
+      {
+        label: '卡片数据录入',
+        name: 'CardData',
+        path: '/card/data'
+      },
+      {
+        label: '创建卡片',
+        name: 'CardAdd',
+        path: '/card/add'
+      }
+    ]
+  }
 ]
 export default menu
